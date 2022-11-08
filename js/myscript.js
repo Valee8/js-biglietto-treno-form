1aebc4c013b535a50809f5749e1b656602477363
Vale <oirelav95@gmail.com>
2022-11-08 16:49:36 +0100

    aggiungo funzione che generi i risultati

--- js/myscript.js
+++ js/myscript.js
@@ -27,3 +27,38 @@ const scontoOver = 0.4;
 
 // Variabile che contiene il prezzo del biglietto
 let prezzoBiglietto;
+
+addButton.addEventListener("click", 
+
+    function() {
+
+        nomeInserito = inputName.value;
+
+        kmInseriti = inputKm.value;
+
+        nome.innerHTML = nomeInserito;
+
+        chilometri.innerHTML = kmInseriti;
+
+        prezzoBiglietto = prezzoAlKm * kmInseriti;
+
+        if (select.value === "minorenne") {
+            prezzoBiglietto -= prezzoBiglietto * scontoMinorenni;
+
+            console.log("prezzo biglietto minorenni: ", prezzoBiglietto);
+
+        } 
+        else if (select.value === "over") {
+            prezzoBiglietto -= prezzoBiglietto * scontoOver;
+            console.log("prezzo biglietto over 65: ", prezzoBiglietto);
+
+        } 
+        else {
+            prezzoBiglietto = prezzoBiglietto;
+            console.log("prezzo biglietto non scontato: ", prezzoBiglietto);
+
+        }
+
+    }
+
+);
